53940dfeff463f93ceefed5d27d699b8e6457901
Aidosgd <aidosgd@gmail.com>
2017-06-26 11:27:36 +0600

    sd

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -59,9 +59,9 @@
         </div>
     </div>
 
-    <nav class="navbar navbar-default" style="display: none">
+    <nav class="navbar navbar-default">
         <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header visible-xs">
+        <div class="navbar-header hidden">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
