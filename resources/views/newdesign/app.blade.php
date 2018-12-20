3b36f62f53618510283e244dc013958ef8535c10
Aidos <aidosgd@gmail.com>
2018-12-20 11:02:18 +0600

    f

--- resources/views/newdesign/app.blade.php
+++ resources/views/newdesign/app.blade.php
@@ -272,6 +272,12 @@
         <div><img src="https://mc.yandex.ru/watch/34783985" style="position:absolute; left:-9999px;" alt="" /></div>
     </noscript>
     <!-- /Yandex.Metrika counter -->
+
+    <!-- BEGIN JIVOSITE CODE -->
+    <script type='text/javascript'>
+        (function(){ var widget_id = 'q5pj7WkUOl';var d=document;var w=window;function l(){var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true;s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+    </script>
+    <!-- END JIVOSITE CODE -->
 </div>
 
 </body>
