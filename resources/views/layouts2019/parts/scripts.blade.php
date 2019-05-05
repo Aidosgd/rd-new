b2d01308b199e3af43e9ed968cbaf04d6c09c4ff
Aidosgd <aidosgd@gmail.com>
2019-05-05 15:30:19 +0600

    download

--- resources/views/layouts2019/parts/scripts.blade.php
+++ resources/views/layouts2019/parts/scripts.blade.php
@@ -36,7 +36,7 @@
             });
 
             myMap.geoObjects
-                .add(myPlacemark)
+            .add(myPlacemark)
         }
 
         new WOW().init();
@@ -54,8 +54,8 @@
 <!-- {literal} -->
 <script type='text/javascript'>
     window['liv' + 'eT' + 'e' + 'x'] = true,
-        window['live' + 'T' + 'e' + 'xI' + 'D'] = 82515,
-        window['li' + 'veTex' + '_obj' + 'ect'] = true;
+    window['live' + 'T' + 'e' + 'xI' + 'D'] = 82515,
+    window['li' + 'veTex' + '_obj' + 'ect'] = true;
     (function () {
         var t = document['creat' + 'eE' + 'lem' + 'ent']('script');
         t.type = 'text/javascript';
@@ -72,29 +72,83 @@
 
 <!-- BEGIN JIVOSITE CODE -->
 <script type='text/javascript'>
-    (function () {
-        var widget_id = 'q5pj7WkUOl';
-        var d = document;
-        var w = window;
+    if ($(window).width()>767){
+        (function () {
+            var widget_id = 'q5pj7WkUOl';
+            var d = document;
+            var w = window;
 
-        function l() {
-            var s = document.createElement('script');
-            s.type = 'text/javascript';
-            s.async = true;
-            s.src = '//code.jivosite.com/script/widget/' + widget_id;
-            var ss = document.getElementsByTagName('script')[0];
-            ss.parentNode.insertBefore(s, ss);
-        }
-
-        if (d.readyState == 'complete') {
-            l();
-        } else {
-            if (w.attachEvent) {
-                w.attachEvent('onload', l);
-            } else {
-                w.addEventListener('load', l, false);
+            function l() {
+                var s = document.createElement('script');
+                s.type = 'text/javascript';
+                s.async = true;
+                s.src = '//code.jivosite.com/script/widget/' + widget_id;
+                var ss = document.getElementsByTagName('script')[0];
+                ss.parentNode.insertBefore(s, ss);
             }
-        }
-    })();
+
+            if (d.readyState == 'complete') {
+                l();
+            } else {
+                if (w.attachEvent) {
+                    w.attachEvent('onload', l);
+                } else {
+                    w.addEventListener('load', l, false);
+                }
+            }
+        })();
+    }
 </script>
 <!-- END JIVOSITE CODE -->
+<style type="text/css">
+    .btn-whatsupp { 
+        position: fixed; bottom: 100px; right: 25px; display: block; width: 70px; height: 70px; z-index: 9; animation-name: animation; animation-duration: 3s; animation-iteration-count: infinite; animation-fill-mode: both; animation-delay: 2s; -webkit-transition: 250ms; -moz-transition: 250ms; -ms-transition: 250ms; -o-transition: 250ms; transition: 250ms; }
+        .btn-whatsupp img { width: 100%; height: auto; }
+        .trubka-mobile { display: none;}
+        @media screen and (max-width: 767px) { 
+            .btn-whatsupp { width: 45px; height: 45px; bottom: 90px; right: 20px; animation: none; } 
+            .trubka-mobile {display: block;opacity: 1;width: 45px !important;height: 45px !important;position: fixed!important;cursor: pointer!important;background: #13e7ff;bottom: 20px;margin-bottom: 0px;right: auto;right: 20px; border-radius: 50%;margin-bottom: 0px;box-shadow: 0px 0px 5px rgba(22, 94, 99,0.8);}
+            .trubka-mobile .icon {width: 25px;height: 25px;background: url(/images/tek2.png) no-repeat left top;background-size: 100% 100%;margin-top: 9px;
+    margin-left: 10px;-webkit-animation-name: rotation1;-webkit-animation-duration: 5s;-webkit-animation-iteration-count: infinite;-webkit-animation-timing-function: linear;-moz-nimation-name: rotation1;-moz-animation-duration: 5s;-moz-animation-iteration-count: infinite;-moz-animation-timing-function: linear;-o-animation-name: otation1;-o-animation-duration: 5s;-o-animation-iteration-count: infinite;-o-animation-timing-function: linear;animation-name: rotation1;animation-duration:5s;animation-iteration-count: infinite;animation-timing-function: linear;
+            }
+        }
+        -webkit-keyframes rotation1{
+            47% {transform:rotate(0deg)}
+            50% {transform:rotate(15deg)}
+            53% {transform:rotate(0deg)}
+            54% {transform:rotate(15deg)}
+            57% {transform:rotate(0deg)}
+            60% {transform:rotate(15deg)}
+            63% {transform:rotate(0deg)}
+            66% {transform:rotate(15deg)}
+            69% {transform:rotate(0deg)}
+            72% {transform:rotate(15deg)}
+            75% {transform:rotate(0deg)}
+        }
+        @keyframes rotation1{
+            47% {transform:rotate(0deg)}
+            50% {transform:rotate(15deg)}
+            53% {transform:rotate(0deg)}
+            54% {transform:rotate(15deg)}
+            57% {transform:rotate(0deg)}
+            60% {transform:rotate(15deg)}
+            63% {transform:rotate(0deg)}
+            66% {transform:rotate(15deg)}
+            69% {transform:rotate(0deg)}
+            72% {transform:rotate(15deg)}
+            75% {transform:rotate(0deg)}
+        }
+
+        @keyframes animation { 20% { -webkit-transform: rotate(360deg); -moz-transform: rotate(360deg); -ms-transform: rotate(360deg); -o-transform: rotate(360deg); transform: rotate(360deg); }
+        35% { bottom: 110px; }
+        50% { bottom: 100px; }
+        55% { bottom: 110px; }
+        60% { bottom: 100px; }
+        75% { bottom: 110px; }
+        80% { bottom: 100px; }
+        85% { bottom: 110px; }
+        100% { bottom: 100px; -webkit-transform: rotate(360deg); -moz-transform: rotate(360deg); -ms-transform: rotate(360deg); -o-transform: rotate(360deg); transform: rotate(360deg); } }
+
+    </style>
+    <a href="https://api.whatsapp.com/send?phone=77071975091" class="btn-whatsupp" target="_blank"><img src="/images/whatsapp.svg" alt=""></a>
+    <a href="tel:+7 747 488 29 12"><div class="trubka-mobile" style="z-index:100;"><div class="icon tricon" id="tricon"></div></div></a>
